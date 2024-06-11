5b66e56dd1a63436230ff42457af0a1e93eef85a
CozmaAndrei <cozma.andrei91@gmail.com>
2024-06-11 16:07:56 +0300

    added new file named weather.py

---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import datetime
+
+def get_weather(city):
+    API_KEY = 'ed7eb50b2d1d8ed53e4627829cf38ee2'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+    try:
+        status = requests.get(url).json()
+        data = status['weather'][0]['description']
+        temperature = round(status['main']['temp'] - 273.15, 1)
+        min_temp = round(status['main']['temp_min'] - 273.15, 1)
+        max_temp = round(status['main']['temp_max'] - 273.15, 1)
+        sunrise_unix = status['sys']['sunrise']
+        sunrise_time = datetime.fromtimestamp(sunrise_unix).strftime('%H:%M')
+        return data, temperature, min_temp, max_temp, sunrise_time
+        # return data
+    except Exception as e:
+        return None
+        
+    
+
+# if __name__ == '__main__':
+#     city = input('Enter city: ')
+#     get_weather(city)
+
